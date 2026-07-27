4bb7818cf81aaa996424d84a16922e9fc7373f9a
maxblak37-afk <maxblak37@gmail.com>
2026-07-27 21:26:48 +0300

    Change lookAhead to 22.f (jump 2 pixels before hitbox)

--- src/main.cpp
+++ src/main.cpp
@@ -480,7 +480,7 @@ class $modify(TrajectoryBot, PlayLayer) {
         bool mustJump = false;
         bool safeToJump = true;
         
-        float lookAhead = 31.f;
+        float lookAhead = 22.f;
         float jumpDist = 130.f; 
         
         auto pBox = m_player1->boundingBox();
