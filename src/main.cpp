1933f4e4ea06c7d05296e7dd01d27e071164898b
maxblak37-afk <maxblak37@gmail.com>
2026-07-27 21:10:23 +0300

    Revert bot hitboxes to original size and tweak jump distance

--- src/main.cpp
+++ src/main.cpp
@@ -480,15 +480,10 @@ class $modify(TrajectoryBot, PlayLayer) {
         bool mustJump = false;
         bool safeToJump = true;
         
-        float lookAhead = 45.f;
-        float jumpDist = 105.f; 
+        float lookAhead = 40.f;
+        float jumpDist = 130.f; 
         
         auto pBox = m_player1->boundingBox();
-        // Shrink player box to match actual GD death hitbox (much smaller than visual)
-        pBox.origin.x += 9.f;
-        pBox.origin.y += 9.f;
-        pBox.size.width -= 18.f;
-        pBox.size.height -= 18.f;
         
         CCRect groundPathBox = { pBox.origin.x, pBox.origin.y, lookAhead, pBox.size.height };
         CCRect landingBox = { pBox.origin.x + jumpDist, pBox.origin.y, pBox.size.width, pBox.size.height };
@@ -502,11 +497,6 @@ class $modify(TrajectoryBot, PlayLayer) {
                 // Fast exit if object is too far behind or too far ahead
                 if (gBox.origin.x < pBox.origin.x - 100.f) continue;
                 if (gBox.origin.x > pBox.origin.x + 200.f) continue;
-                
-                // Shrink bounding box to approximate the REAL GD spike hitbox
-                gBox.origin.x += 10.f;
-                gBox.size.width -= 20.f;
-                gBox.size.height -= 15.f;
                 
                 if (groundPathBox.intersectsRect(gBox)) {
                     mustJump = true;
